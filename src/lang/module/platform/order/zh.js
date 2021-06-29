9901e9350362ded2e1622eecec32127b9e61c5aa
Zhang XiaoFei <1326336909@qq.com>
2021-06-29 21:59:18 +0800

    短信 与 订单

--- src/lang/module/platform/order/zh.js
+++ src/lang/module/platform/order/zh.js
@@ -4,10 +4,9 @@ export const order = {
   view: '查看订单',
   close: '结束课程',
   cancel: '取消订单',
+  delete: '删除订单',
 
   basic_info: '订单信息',
-  address_info: '收货信息',
-  course_present_info: '购课礼包',
 
   order_no: '订单号',
   nickname: '购买用户',
@@ -16,9 +15,7 @@ export const order = {
   pay_type: '支付类型',
   send: '发货',
   pay_status: '支付状态',
-  order_status: '礼包状态',
+  order_status: '订单状态',
   create_time: '支付时间',
-  money_total: '收入(元):',
-  description: '礼包发货后，获取运单状态',
 
 }
--- src/views/module/order/list.vue
+++ src/views/module/order/list.vue
@@ -22,8 +22,14 @@
             </el-input>
           </div>
           <div>
-            <el-input v-model="dataForm.member_nickname" :placeholder="$t('common.please_input') + $t('member.nickname')" clearable>
+            <el-input v-model="dataForm.member_username" :placeholder="$t('common.please_input') + $t('member.username')" clearable>
             </el-input>
+          </div>
+          <div>
+            <el-select v-model="dataForm.order_status" :placeholder="$t('common.please_select') + $t('order.order_status')" clearable>
+              <el-option :label="$t('common.all')" value=""></el-option>
+              <el-option v-for="(v,k) in orderList" :label="v.title" :key="k" :value="v.id"></el-option>
+            </el-select>
           </div>
           <div>
             <el-button icon="el-icon-search" @click="getDataList()">
@@ -101,12 +107,16 @@
                 {{ $t('order.view') }}
               </el-button>
 
-              <el-button v-if="isAuth('module:order:close') && scope.row.order_status.value == 0 && scope.row.pay_status.value == 1" type="primary" icon="el-icon-s-promotion" @click="$router.push({name: 'module_order_course_send', query: {order_id: scope.row.id, member_id: scope.row.member_id}})">
+              <el-button v-if="isAuth('module:order:close') && scope.row.order_status.value == 1 && scope.row.pay_status.value == 1" type="warning" icon="el-icon-close" @click="$router.push({name: 'module_order_course_send', query: {order_id: scope.row.id, member_id: scope.row.member_id}})">
                 {{ $t('order.close') }}
               </el-button>
 
-              <el-button v-if="isAuth('module:order:cancel') && scope.row.order_status.value == 0 && scope.row.pay_status.value == 1" type="danger" icon="el-icon-delete" @click="handleCancel(scope.row.id)">
+              <el-button v-if="isAuth('module:order:cancel') && (scope.row.order_status.value == 0 || scope.row.order_status.value == 1) && scope.row.pay_status.value == 1" type="warning" icon="el-icon-switch-button" @click="handleCancel(scope.row.id)">
                 {{ $t('order.cancel') }}
+              </el-button>
+
+              <el-button v-if="isAuth('module:order:delete') && scope.row.order_status.value == 3" type="danger" icon="el-icon-delete" @click="deleteHandle(scope.row.id)">
+                {{ $t('order.delete') }}
               </el-button>
             </template>
           </el-table-column>
@@ -134,10 +144,16 @@
     data() {
       return {
         model: 'order',
+        orderList: [
+          {'id': 0, 'title': '待学习'},
+          {'id': 1, 'title': '学习中'},
+          {'id': 2, 'title': '已完成'},
+          {'id': 3, 'title': '已关闭'},
+        ],
         dataForm: [
           'order_no',
           'courseware_title',
-          'member_nickname',
+          'member_username',
         ]
       };
     },
